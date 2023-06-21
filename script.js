728cd53df7d9fb74003008db4b55926ce3e070b2
Rajat24111994 <rajatmohangpt3@gmail.com>
2023-06-21 19:01:26 +0530

    done

---
+++ script.js
@@ -0,0 +1,20 @@
+function myfunction(){
+    document.getElementById("mytopnav").classList.toggle("responsive");
+  }
+  
+  function sub(){
+    event.preventDefault()
+    alert("hello")
+    emailjs.send("service_qanxv6w","template_5g7ruee",{
+      from_name: "Rajat",
+      message: "Hello !",
+      },"h8s4g7h4yAgMruUze");   
+  }
+  GitHubCalendar(".calendar", "Rajat24111994");
+  // or enable responsive functionality
+  GitHubCalendar(".calendar", "Rajat24111994", { responsive: true });
+  function resume(){
+  window.open("https://drive.google.com/file/d/1opUM57d_nAP79bhtp4pt9JmgjPraEWan/view?usp=sharing", "_blank");
+  }
+  
+  
